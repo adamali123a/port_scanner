715ce821d5cbc8245d34cd1bbd941a443cb60f27
adamali123a <54318139+adamali123a@users.noreply.github.com>
2020-08-09 15:13:55 +0300

    Add files via upload

---
+++ portsc.py
@@ -0,0 +1,23 @@
+import socket
+moscow = r"""
+ __  __
+|  \/  | ___  ___  ___ _____      __
+| |\/| |/ _ \/ __|/ __/ _ \ \ /\ / /
+| |  | | (_) \__ \ (_| (_) \ V  V /
+|_|  |_|\___/|___/\___\___/ \_/\_/
+"""
+print("\033[0;32m",moscow)
+print("\n")
+
+l = input("Lyera ip addressk banwsa:  ")
+try:
+    for p in range(1,10000):
+        a = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        e = a.connect_ex((l,p))
+        if e == 0:
+            ss = socket.getservbyport(p)
+            print("porte {} joaraky ({}) krawya".format(p,ss))
+
+except:
+    print("aw shta bony nya")
