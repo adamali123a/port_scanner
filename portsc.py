d448d6558570b18ef4c4e646dbd4fea9cdfed99d
adamali123a <54318139+adamali123a@users.noreply.github.com>
2020-08-09 15:29:01 +0300

    Update portsc.py

--- portsc.py
+++ portsc.py
@@ -10,7 +10,7 @@ print("\033[0;32m",moscow)
 print("\n")
 print("script by moscow")
 print("\n")
-l = input("Lyera ip addressk banwsa:  ")
+l = input("Lyera ipka banwsa:  ")
 try:
     for p in range(1,10000):
         a = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
